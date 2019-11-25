8217b8de631dbd6ff196e9f5cdd216dd465a6b6c
zlira <olesia.hr@gmail.com>
2019-11-25 10:59:20 +0200

    Started to work on announcements

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import SiteNavigation from './SiteNavigation'
 import About from './About'
+import Announcements from './Announcements'
 
 function App() {
   return (<div className="site container">
     <SiteNavigation/>
     <About/>
+    <Announcements/>
   </div>)
 }
 
