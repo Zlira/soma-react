7bafdcb2fa3e0af6007d23c23eb59d9010144387
zlira <olesia.hr@gmail.com>
2020-01-20 16:56:34 +0200

    Updates

---
+++ precache-manifest.06a46ddc400588f559b1fff3320f8648.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57cdf180961d6c4ffa33e981d738c9fb",
+    "url": "/soma-react/index.html"
+  },
+  {
+    "revision": "0ef2dda08db177a00f97",
+    "url": "/soma-react/static/css/main.1048ca3f.chunk.css"
+  },
+  {
+    "revision": "7b1acfab4854b14da6aa",
+    "url": "/soma-react/static/js/2.9fe6ff58.chunk.js"
+  },
+  {
+    "revision": "0ef2dda08db177a00f97",
+    "url": "/soma-react/static/js/main.262b454d.chunk.js"
+  },
+  {
+    "revision": "f01df57a1c36971e9ac2",
+    "url": "/soma-react/static/js/runtime-main.2d294005.js"
+  },
+  {
+    "revision": "5c90b60dd2e510e62d0f8525c0edf84b",
+    "url": "/soma-react/static/media/3kovtky.5c90b60d.png"
+  },
+  {
+    "revision": "4b13bc5c0dfd511ba1d6ffb864a17f70",
+    "url": "/soma-react/static/media/bush.4b13bc5c.png"
+  },
+  {
+    "revision": "118df448d4f68ac923c87e9d59ffed06",
+    "url": "/soma-react/static/media/come.118df448.png"
+  },
+  {
+    "revision": "e916b85670c5c69093f23f7decfa5f6b",
+    "url": "/soma-react/static/media/cracks.e916b856.png"
+  },
+  {
+    "revision": "45d80bcc77976222b2691463d779ff0e",
+    "url": "/soma-react/static/media/donate.45d80bcc.png"
+  },
+  {
+    "revision": "3a67a5dfb02ffebadb5d6d72779aeace",
+    "url": "/soma-react/static/media/litachok.3a67a5df.png"
+  },
+  {
+    "revision": "4f1134d23ac8fe6fddd6e1aa0dc27a53",
+    "url": "/soma-react/static/media/maletilo.4f1134d2.png"
+  },
+  {
+    "revision": "c62efdc0ea48de4a221f9afe26c3d1b9",
+    "url": "/soma-react/static/media/mega.c62efdc0.png"
+  },
+  {
+    "revision": "391af472e8cd6f64d3be0990d29e08a9",
+    "url": "/soma-react/static/media/next.391af472.png"
+  },
+  {
+    "revision": "14f0bc2102711c84e0f235a4dcd3cb30",
+    "url": "/soma-react/static/media/prev.14f0bc21.png"
+  },
+  {
+    "revision": "3c9b5bc8fb43dc4a084425009d2fe139",
+    "url": "/soma-react/static/media/spring.3c9b5bc8.png"
+  },
+  {
+    "revision": "f9ffc8c5da6175e9bc0647e4152508fc",
+    "url": "/soma-react/static/media/sticks.f9ffc8c5.png"
+  },
+  {
+    "revision": "cd799906bbbc3831b19d60b024254857",
+    "url": "/soma-react/static/media/tilo.cd799906.png"
+  }
+]);
