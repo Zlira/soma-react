f57f8d8a5cd54d4513c2d2b1ea9927bd871ae3c3
dfkthsq <dfkthsq@gmail.com>
2020-01-22 16:10:28 +0200

    Staff Section Added

--- src/Components/SiteNavigation/SiteNavigation.js
+++ src/Components/SiteNavigation/SiteNavigation.js
@@ -1,10 +1,9 @@
-import React, {useState} from "react";
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import { Link } from "react-scroll";
 
 import "./SiteNavigation.css";
-
 
 function NavLink({ href, text, children, onSelect }) {
   return (
@@ -46,10 +45,9 @@ function SocialMediaLink(props) {
 function SiteNavigation() {
   const navItems = [
     { href: "announcements", text: "анонси" },
-    // { href: "#workshop", text: "майстерні" },
     { href: "participation", text: "участь" },
     { href: "calendar", text: "публічні події" },
-    // { href: "#masters", text: "майстри_ні" },
+    { href: "staff", text: "колектив" },
     {
       href: "contacts",
       text: "контакти",
@@ -67,18 +65,22 @@ function SiteNavigation() {
       )
     }
   ];
-  const [expanded, setExpanded] = useState(false)
+  const [expanded, setExpanded] = useState(false);
   return (
-    <Navbar collapseOnSelect fixed="top"
+    <Navbar
+      collapseOnSelect
+      fixed="top"
       expanded={expanded}
       onToggle={() => setExpanded(!expanded)}
-      expand="sm" className='justify-content-end'>
+      expand="sm"
+      className="justify-content-end"
+    >
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse className='justify-content-end'>
+      <Navbar.Collapse className="justify-content-end">
         <Nav>
           {navItems.map(item => (
             <Nav.Item key={item.href}>
-              <NavLink {...item}  onSelect={() => setExpanded(false)}/>
+              <NavLink {...item} onSelect={() => setExpanded(false)} />
             </Nav.Item>
           ))}
         </Nav>
