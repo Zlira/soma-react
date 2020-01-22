14087c1423bb62775a55b720b43dc19e346eb4f6
dfkthsq <dfkthsq@gmail.com>
2020-01-22 20:00:12 +0200

    Staff css added

--- src/Components/Staff/index.js
+++ src/Components/Staff/index.js
@@ -6,12 +6,12 @@ import "./index.css";
 function Staff(props) {
   const members = memberInfo.map((el, i) => <Member {...el} key={i} />);
   return (
-    <section id="staff" className="entry mb-5 participation">
+    <section id="staff" className="entry mb-5 staff">
       <header className="entry-header">
         <h2 className="entry-title">Колектив</h2>
       </header>
       <div className="entry-content">
-        <div className="row justify-content-center">{members.slice(0, 2)}</div>
+        <div className="row justify-content-left">{members.slice(0, 2)}</div>
         <div className="mb-5 row justify-content-center">
           {members.slice(2)}
         </div>
