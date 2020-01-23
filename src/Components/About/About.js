563e3fa5ed3af516250d599cc2aa979c27bd020b
dfkthsq <dfkthsq@gmail.com>
2020-01-23 20:10:55 +0200

    What-is-it css

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,63 +1,60 @@
 import React, { useState } from "react";
-import Collapse from 'react-bootstrap/Collapse';
+import Collapse from "react-bootstrap/Collapse";
 
-import {
-  LetterS,
-  LetterO,
-  LetterM,
-  LetterA
-} from "./SomaLetters.js";
+import { LetterS, LetterO, LetterM, LetterA } from "./SomaLetters.js";
 
-import banerImg from './imgs/soma_banerok.svg'
-import hairImg from './imgs/volossia.png'
-import circleImg from './imgs/kruzhok.svg'
-import stickImg from './imgs/palychka.svg'
-import heartImg from './imgs/serce.svg'
+import banerImg from "./imgs/soma_banerok.svg";
+import hairImg from "./imgs/volossia.png";
+import circleImg from "./imgs/kruzhok.svg";
+import stickImg from "./imgs/palychka.svg";
+import heartImg from "./imgs/serce.svg";
 
 import "./About.css";
-
 
 function SiteDescription() {
   return (
     <div className="site-description">
+      <div className="col-1 baner-col"></div>
       <div className="row">
-        <p className="col-md">танцююча спільнота самосвідомих та активних громадян_ок</p>
-        <p className="col-md">альтернативне дозвілля, в основі якого експеримет та практики рівності</p>
+        <p className="col-md-4 text-uppercase mb-5 ">
+          танцююча спільнота самосвідомих та активних громадян_ок
+        </p>
+        <p className="col-md-4 text-uppercase mb-5">
+          альтернативне дозвілля, в основі якого експеримет та практики рівності
+        </p>
       </div>
       <div className="row">
-        <p className="col-md">зв'язок з тілом, як база для мистецького дослідження та здорового життя</p>
-        <p className="col-md">інтсрументи та знання для тілесного і творчого розвитку</p>
+        <p className="col-md-4 text-uppercase mb-5">
+          зв'язок з тілом, як база для мистецького дослідження та здорового
+          життя
+        </p>
+        <p className="col-md-4 text-uppercase mb-5">
+          інтсрументи та знання для тілесного і творчого розвитку
+        </p>
+
+        <p className="col-md-8">
+          Soma. майстерня – це відкрите експериметальне та продуктивне мистецьке
+          середовище, що існує в контексті міста. Бачимо своєю місією створити
+          нову культуру тілесності, в центрі якої будуть права людини та вільне
+          дослідження. Нам близькі питання тіла в урбаністичному середовищі та
+          DIY культура. Основні напрямки діяльності майстерні – дослідження в
+          сучасному танці, перформансі, дизайні, та експериментальний самвидав.
+        </p>
       </div>
-      <p>
-        Soma. майстерня – це відкрите експериметальне та продуктивне мистецьке
-        середовище, що існує в контексті міста. Бачимо своєю місією створити
-        нову культуру тілесності, в центрі якої будуть права людини та вільне
-        дослідження. Нам близькі питання тіла в урбаністичному середовищі та DIY
-        культура. Основні напрямки діяльності майстерні – дослідження в
-        сучасному танці, перформансі, дизайні, та експериментальний самвидав.
-      </p>
     </div>
   );
 }
 
-
-function Button({text, link, className}) {
-  className = className || ''
+function Button({ text, link, className }) {
+  className = className || "";
   return (
-      <div className={'heart-button ' + className}>
-        <a
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="heart-button-text">{text}</span>
-          <img
-            src={heartImg}
-            alt={text}
-          />
-        </a>
-      </div>
-    )
+    <div className={"heart-button " + className}>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <span className="heart-button-text">{text}</span>
+        <img src={heartImg} alt={text} />
+      </a>
+    </div>
+  );
 }
 
 function Marquee() {
@@ -86,21 +83,16 @@ function Marquee() {
 }
 
 function col(classes, children) {
-  const className = classes.reduce((acc, val) => acc + ' ' + val,  '')
-  return <div className={className}>{children}</div>
+  const className = classes.reduce((acc, val) => acc + " " + val, "");
+  return <div className={className}>{children}</div>;
 }
 
-function LeftCol({children}) {
-  const classes = [
-    'left-col',
-    'col-8',
-    'col-lg-4',
-  ]
-  return col(classes, children)
+function LeftCol({ children }) {
+  const classes = ["left-col", "col-8", "col-lg-4"];
+  return col(classes, children);
 }
 
-
-function CenterCol({children}) {
+function CenterCol({ children }) {
   const classes = [
     "center-col",
     "col-3",
@@ -108,12 +100,12 @@ function CenterCol({children}) {
     "d-flex",
     "flex-column",
     "align-items-center",
-    "justify-content-end",
-  ]
-  return col(classes, children)
+    "justify-content-end"
+  ];
+  return col(classes, children);
 }
 
-function RightCol({children}) {
+function RightCol({ children }) {
   const classes = [
     "right-col",
     "d-none",
@@ -121,33 +113,40 @@ function RightCol({children}) {
     "flex-column",
     "col-6",
     "align-items-center",
-    "justify-content-end",
-  ]
-  return col(classes, children)
+    "justify-content-end"
+  ];
+  return col(classes, children);
 }
 
 export default function About() {
-  let [descriptionVisible, setDescriptionVisible] = useState(false)
+  let [descriptionVisible, setDescriptionVisible] = useState(false);
   return (
     <section id="home" className="entry mb-5">
       <div className="row no-gutters what-is-it-row">
         <div className="col-1 banner-col">
-          <img src={banerImg}  className="banner-img" alt="soma" />
+          <img src={banerImg} className="banner-img" alt="soma" />
         </div>
         <LeftCol>
-          <p className="what-is-it" onClick={() => setDescriptionVisible(!descriptionVisible)}>що це?</p>
+          <p
+            className="what-is-it"
+            onClick={() => setDescriptionVisible(!descriptionVisible)}
+          >
+            що це?
+          </p>
         </LeftCol>
         <CenterCol>
           <LetterS />
           <LetterO />
         </CenterCol>
         <RightCol>
-          <LetterA/>
+          <LetterA />
         </RightCol>
       </div>
       <Collapse in={descriptionVisible}>
-        <div className="site-description-container">
-          <SiteDescription/>
+        <div className="row justify-content-end">
+          <div className="site-description-container col-12 col-md-11">
+            <SiteDescription />
+          </div>
         </div>
       </Collapse>
       <Collapse in={!descriptionVisible}>
@@ -157,8 +156,8 @@ export default function About() {
               <img src={hairImg} className="hair-img" alt="" />
             </LeftCol>
             <CenterCol>
-              <img src={stickImg} className="stick-img" alt=""/>
-              <img src={circleImg} className="circle-img" alt=""/>
+              <img src={stickImg} className="stick-img" alt="" />
+              <img src={circleImg} className="circle-img" alt="" />
             </CenterCol>
             <RightCol></RightCol>
           </div>
@@ -166,27 +165,31 @@ export default function About() {
       </Collapse>
       <div className="row no-gutters come-row justify-content-end">
         <LeftCol>
-          <Button text="прийти"
+          <Button
+            text="прийти"
             className="heart-button-come"
-            link="https://forms.gle/TBfYPvjDfvb1cb59A"/>
+            link="https://forms.gle/TBfYPvjDfvb1cb59A"
+          />
         </LeftCol>
         <CenterCol></CenterCol>
         <RightCol></RightCol>
       </div>
       <div className="row no-gutters donate-row justify-content-end">
         <LeftCol>
-          <Button text="donate"
+          <Button
+            text="donate"
             className="heart-button-donate"
-            link="https://privatbank.ua/sendmoney?payment=a101e4603b"/>
+            link="https://privatbank.ua/sendmoney?payment=a101e4603b"
+          />
         </LeftCol>
         <CenterCol>
-          <LetterM/>
+          <LetterM />
         </CenterCol>
         <RightCol></RightCol>
       </div>
       <div className="row d-flex d-lg-none no-gutters md-bottom-row justify-content-end">
         <CenterCol>
-          <LetterA/>
+          <LetterA />
         </CenterCol>
       </div>
       <Marquee />
